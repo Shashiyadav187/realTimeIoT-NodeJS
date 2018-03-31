aec525ad42023b7ef36218f528c5705a4330d43d
sumel <sumel124@gmail.com>
2018-03-31 17:08:21 -0500

    Add sequelize

--- realTimeNodeJS-db/index.js
+++ realTimeNodeJS-db/index.js
@@ -1,17 +1,23 @@
 'use strict'
 
-const setupDatabase = require('../lib/db')
-const setupAgentModel = require('../models/agent')
-const setupMetricModel = require('../models/metric')
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
 
 module.exports = async function (config) {
-  const sequealize = setupDatabase(config)
+  const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
-  const MatricModel = setupMetricModel(config)
-  AgentModel.hasMany(MatricModel)
-  MatricModel.belongsTo(AgentModel)
-  await sequealize.authenticate()
-  // sequealize.async()
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
+
   const Agent = {}
   const Metric = {}
 
